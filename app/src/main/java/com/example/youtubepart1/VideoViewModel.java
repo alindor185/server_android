8e5b4ac9dcf6010c32db77e76dc94e4d26ebaebd
alindor185 <alindor185@gmail.com>
2024-09-03 13:55:13 +0300

    Add VideoViewModel for managing video data operations in the ViewModel layer.

---
+++ app/src/main/java/com/example/youtubepart1/VideoViewModel.java
@@ -0,0 +1,42 @@
+package com.example.youtubepart1;
+
+import android.content.Context;
+
+import androidx.lifecycle.ViewModel;
+
+import java.util.List;
+
+public class VideoViewModel extends ViewModel {
+    VideoRepository repository;
+    public VideoViewModel(Context context) {
+        repository = new VideoRepository(context);
+    }
+
+    public List<Video> getVideos() {
+        return repository.getVideos();
+    }
+
+    public Video getVideo(int id) {
+        return repository.getVideo(id);
+    }
+
+    public void insert(Video video) {
+        repository.insert(video);
+    }
+    public void changeName(int id, String newName) {
+        repository.changeName(id, newName);
+    }
+    public void delete(Video video) {
+        repository.delete(video);
+    }
+    public void update(Video video) {
+        repository.update(video);
+    }
+    public List<Video> getVideosByUsername(String username) {
+        return repository.getVideosByUsername(username);
+    }
+
+    public void deleteAll() {
+        repository.deleteAll();
+    }
+}
