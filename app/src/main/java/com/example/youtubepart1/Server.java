c9c81809c24be09614901eb81f420856fee6018e
alindor185 <130838395+alindor185@users.noreply.github.com>
2024-09-03 14:04:27 +0300

    Enhance Server Integration and User Management
    - Implemented server communication methods for handling user authentication, profile updates, and CRUD operations for videos and comments.
    - Added new endpoints for user registration, login, and profile management.

--- app/src/main/java/com/example/youtubepart1/Server.java
+++ app/src/main/java/com/example/youtubepart1/Server.java
@@ -7,18 +7,19 @@ import android.net.Uri;
 
 import androidx.annotation.NonNull;
 
+import com.google.gson.Gson;
+
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.ClassicHttpResponse;
 import org.apache.hc.core5.http.ContentType;
 import org.apache.hc.core5.http.HttpEntity;
 import org.apache.hc.core5.http.HttpException;
-import org.apache.hc.core5.http.ParseException;
 import org.apache.hc.core5.http.io.HttpClientResponseHandler;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -28,18 +29,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 public class Server {
     private Context context;
@@ -57,7 +51,12 @@ public class Server {
         } catch (JSONException e) {
             throw new RuntimeException(e);
         }
-        return post("users", jsonObject.toString());
+        try {
+            return new JSONObject(post("users", jsonObject.toString()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     /** returns the authorization token too **/
@@ -69,32 +68,74 @@ public class Server {
         } catch (JSONException e) {
             throw new RuntimeException(e);
         }
-        return post("tokens", jsonObject.toString());
+        try {
+            return new JSONObject(post("tokens", jsonObject.toString()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    public JSONObject editLike(String userName) {
-        String requestBodyData = "";
-        return post("tokens/"+userName+"/posts/:pid/like", requestBodyData);
+    public void editVideo(Video video) {
+        patchWithAuth("users/"+video.getUploaderId()+"/videos/"+video.get_id(), getVideoJson(video).toString(), UserToken.token);
     }
 
-    public JSONObject getUser(String userName, String token) {
-        String requestBodyData = "";
-        return getWithAuth("tokens/"+userName, requestBodyData, token);
+    public void deleteVideo(Video video) {
+        deleteWithAuth("/users/"+video.getUploaderId()+"/videos/"+video.get_id(), "", UserToken.token);
     }
 
-    public JSONObject createVideo(Video video) {
+    public void incrementVideoViews(Video video) {
+        patch("/users/"+video.getUploaderId()+"/videos/"+video.get_id()+"/views", "");
+    }
+
+    public JSONObject updateProfile(User user) {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("email", user.email);
+            jsonObject.put("username", user.userName);
+            jsonObject.put("password", user.password);
+            jsonObject.put("profilePicture", user.image);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            return new JSONObject(patchWithAuth("/users/"+UserToken.id, jsonObject.toString(), UserToken.token));
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void deleteProfile(User user) {
+        deleteWithAuth("/users/"+UserToken.id, "", UserToken.token);
+    }
+
+    public JSONArray getVideos() {
+        try {
+            String videosString = null;
+            while (videosString == null)
+                videosString = get("videos", "");
+            return new JSONArray(videosString);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private JSONObject getVideoJson(Video video) {
         JSONObject jsonObject = new JSONObject();
         try {
             jsonObject.put("title", video.getTitle());
             MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-            if (video.getIsFile())
-                retriever.setDataSource(context, Uri.fromFile(new File(video.getUrl())));
-            else
-                retriever.setDataSource(context, Uri.parse(video.getUrl()));
+            retriever.setDataSource(context, Uri.fromFile(new File(video.getFilePath())));
             String time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
             long timeInMillisec = Long.parseLong(time);
             retriever.release();
             jsonObject.put("duration", timeInMillisec/1000);
+            List<Video.Comment> comments = video.getCommentsList();
+            for (Video.Comment comment : comments)
+                comment.setProfilePic("");
+            jsonObject.put("comments", new JSONArray(new Gson().toJson(comments)));
             JSONObject jsonObject2 = new JSONObject();
             jsonObject2.put("username", video.getUserName());
             jsonObject2.put("profilePicture", "data:image/png;base64,"+video.getProfilePic());
@@ -103,19 +144,13 @@ public class Server {
         } catch (JSONException | IOException e) {
             throw new RuntimeException(e);
         }
-        File videoFile = null;
-        if (!video.getIsFile()) {
-            try {
-                Bitmap bitmap = BitmapUtils.getResizedBitmap(ImageOperations.uriToBitmap(context, Uri.parse(video.getUrl())));
-                File tmp = new File(context.getExternalFilesDir(null), System.currentTimeMillis()+".jpg");
-                FileOutputStream fos = new FileOutputStream(tmp);
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 70, fos);
-                videoFile = tmp;
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        } else
-            videoFile = new File(video.getUrl());
+        return jsonObject;
+    }
+
+    public JSONObject createVideo(Video video) {
+        File videoFile = new File(video.getFilePath());
+
+        JSONObject jsonObject = getVideoJson(video);
 
         File thumbnailFile = new File(context.getExternalFilesDir(null), System.currentTimeMillis()+"2.jpg");
         try {
@@ -128,35 +163,74 @@ public class Server {
         return sendFormData("users/"+UserToken.id+"/videos", jsonObject, UserToken.token, videoFile, "videoFile", thumbnailFile, "thumbnail");
     }
 
-    private static JSONObject post(String subURL, String requestBodyData) {
+    public JSONObject addComment(Video.Comment comment, String videoId) {
+        try {
+            return new JSONObject(postWithAuth("/videos/"+videoId+"/comments", new Gson().toJson(comment), UserToken.token));
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public JSONObject editComment(Video.Comment comment, String videoId) {
+        try {
+            return new JSONObject(patchWithAuth("/videos/"+videoId+"/comments/"+comment.get_id(), new Gson().toJson(comment), UserToken.token));
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public JSONObject deleteComment(Video.Comment comment, String videoId) {
+        try {
+            return new JSONObject(deleteWithAuth("/videos/"+videoId+"/comments/"+comment.get_id(), new Gson().toJson(comment), UserToken.token));
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String post(String subURL, String requestBodyData) {
         return sendRequestToServer(subURL, requestBodyData, null, "POST");
     }
 
-    private static JSONObject get(String subURL, String requestBodyData) {
+    private static String get(String subURL, String requestBodyData) {
         return sendRequestToServer(subURL, requestBodyData, null, "GET");
     }
 
-    private static JSONObject getWithAuth(String subURL, String requestBodyData, String token) {
+    private static String deleteWithAuth(String subURL, String requestBodyData, String token) {
+        return sendRequestToServer(subURL, requestBodyData, token, "DELETE");
+    }
+
+    private static String patch(String subURL, String requestBodyData) {
+        return sendRequestToServer(subURL, requestBodyData, null, "PATCH");
+    }
+
+    private static String patchWithAuth(String subURL, String requestBodyData, String token) {
+        return sendRequestToServer(subURL, requestBodyData, token, "PATCH");
+    }
+
+    private static String getWithAuth(String subURL, String requestBodyData, String token) {
         return sendRequestToServer(subURL, requestBodyData, token, "GET");
     }
 
-    private static JSONObject postWithAuth(String subURL, String requestBodyData, String token) {
+    private static String postWithAuth(String subURL, String requestBodyData, String token) {
         return sendRequestToServer(subURL, requestBodyData, token, "POST");
     }
 
-    private static JSONObject sendRequestToServer(String subURL, String requestBodyData, String authorizationToken, String method) {
+    private static String sendRequestToServer(String subURL, String requestBodyData, String authorizationToken, String method) {
         try {
             URL url = new URL("http://10.0.2.2:5000/api/"+subURL);
             HttpURLConnection httpUrlConnection = (HttpURLConnection) url.openConnection();
             httpUrlConnection.setRequestMethod(method);
-            httpUrlConnection.setDoOutput(true);
+            if (method.equals("POST") || method.equals("PATCH"))
+                httpUrlConnection.setDoOutput(true);
             httpUrlConnection.setRequestProperty("Content-Type", "application/json");
             httpUrlConnection.setRequestProperty("charset", "utf-8");
             if (authorizationToken != null)
                 httpUrlConnection.setRequestProperty("Authorization", "Bearer "+authorizationToken);
-            StringBuilder builder = getStringBuilder(requestBodyData, httpUrlConnection);
-            return new JSONObject(builder.toString());
-        } catch (IOException|JSONException e) {
+            if (method.equals("POST") || method.equals("PATCH"))
+                writeBodyData(httpUrlConnection, requestBodyData);
+            StringBuilder builder = getStringBuilder(httpUrlConnection);
+            return builder.toString();
+        } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
@@ -200,33 +274,20 @@ public class Server {
         }
     }
 
-    @NonNull
-    private static StringBuilder getStringBuilder(String requestBodyData, HttpURLConnection httpUrlConnection) throws IOException {
+    private static void writeBodyData(HttpURLConnection httpUrlConnection, String requestBodyData) throws IOException {
         DataOutputStream dataOutputStream = new DataOutputStream(httpUrlConnection.getOutputStream());
         dataOutputStream.writeBytes(requestBodyData);
+    }
+
+    @NonNull
+    private static StringBuilder getStringBuilder(HttpURLConnection httpUrlConnection) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpUrlConnection.getInputStream()));
         StringBuilder builder = new StringBuilder();
         String stringLine;
         while ((stringLine = bufferedReader.readLine()) != null) {
+            System.out.println(stringLine);
             builder.append(stringLine).append("\n");
         }
         return builder;
-    }
-
-    private static void sendFile(HttpURLConnection connection, File file, String name) {
-        try {
-            OutputStream output = connection.getOutputStream();
-            PrintWriter writer = new PrintWriter(new OutputStreamWriter(output, StandardCharsets.UTF_8), true);
-            writer.append("--").append(Long.toHexString(System.currentTimeMillis())).append("\r\n");
-            writer.append("Content-Disposition: form-data; name=\""+name+"\"; filename=\"").append(file.getName()).append("\"").append("\r\n");
-            writer.append("Content-Type: ").append(URLConnection.guessContentTypeFromName(file.getName())).append("\r\n");
-            writer.append("Content-Transfer-Encoding: binary").append("\r\n");
-            writer.append("\r\n").flush();
-            Files.copy(file.toPath(), output);
-            output.flush(); // Important before continuing with writer!
-            writer.append("\r\n").flush(); // CRLF is important! It indicates end of boundary.
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
